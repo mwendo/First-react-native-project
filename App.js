aee40a5bad80d91e2dc13f624529c9f8d05ebfb6
Micah Wendorf <wendorfmicah0@gmail.com>
2021-05-24 16:11:03 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+  const [animal, setAnimal] = useState("")
+
+  const [list, setList] = useState([
+    {id:"1", animal:"penguins"}, 
+    {id:"2", animal: "gorillas"},
+    {id:"3", animal: "narwhals"},
+    {id:"4", animal: "ligers"},
+    {id:"5", animal: "giant squid"}
+  ])
+
+  const inputStyle = {
+    borderWidth: 2,
+    width: "75%",
+    marginTop: "40%"
+  }
+
+  const onSubmitHandler = () => {
+    let newId = parseInt(list[list.length-1].id) + 1;
+    
+    setList([
+      ...list,
+      {
+        id: `${newId}`,
+        animal: animal
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hey it's me!</Text>
+      <Text>second line!</Text>
+      <TextInput style={inputStyle} onChangeText={(input) => setAnimal(input)} onSubmitEditing={onSubmitHandler}/>
+      <FlatList data={list} renderItem={({item})=><Text>{item.animal}</Text>}/>
       <StatusBar style="auto" />
     </View>
   );
